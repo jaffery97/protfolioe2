0275ae8cc371a3d8f2c24d25017b53e628cc75af
jaffery97 <hasanjafory@gmail.com>
2018-01-05 00:34:47 -0500

    first commit

---
+++ js/omode.js
@@ -0,0 +1,54 @@
+//Education
+function openEdu() {
+    document.getElementById("tab1").style.height = "100%";
+}
+
+function closeEdu() {
+    document.getElementById("tab1").style.height = "0%";
+}
+
+//Experiences
+
+function openExp() {
+    document.getElementById("tab2").style.height = "100%";
+}
+
+function closeExp() {
+    document.getElementById("tab2").style.height = "0%";
+}
+//Contact
+function openCon() {
+    document.getElementById("tab3").style.height = "100%";
+}
+
+function closeCon() {
+    document.getElementById("tab3").style.height = "0%";
+}
+
+//Awards & Certifications
+function openCer() {
+    document.getElementById("tab4").style.height = "100%";
+}
+
+function closeCer() {
+    document.getElementById("tab4").style.height = "0%";
+}
+
+//Projects
+function openPro() {
+    document.getElementById("tab5").style.height = "100%";
+}
+
+function closePro() {
+    document.getElementById("tab5").style.height = "0%";
+}
+
+//Skill
+
+function openSki() {
+    document.getElementById("tab6").style.height = "100%";
+}
+
+function closeSki() {
+    document.getElementById("tab6").style.height = "0%";
+}
